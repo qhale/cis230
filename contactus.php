fe674922f47fd4237ced0d9b1b0b075cefb34627
Queena <Queena@Queenas-Air.hsd1.wa.comcast.net>
2016-01-26 21:01:53 -0800

    Added checkbox, stickied form, added basic error checking

--- contactus.php
+++ contactus.php
@@ -11,10 +11,12 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $question = $_POST['question'];
 $newsletter = $_POST['newsletter'];
+$notify = $_POST['notify'];
 $contact = $_POST['contact']; //radio button choice
-$submit = $_POST['submit'];
+//$submit = $_POST['submit'];
 
 $c_newsletter = $newsletter == "on" ? 'checked="checked"': "";
+$c_notify = $notify == "on" ? 'checked="checked"': "";
 
 $c_email = $contact == "c_email" ? 'checked="checked"': "";
 $c_phone = $contact == "c_phone" ? 'checked="checked"': "";
@@ -23,7 +25,20 @@ $inquiry = array("General Inquiry", "Volunteer", "Other");
 $selected_inquiry = $_POST['inquiry'];
 $select = createSelect('inquiry', $inquiry, $selected_inquiry);
 
+$errors = array();
+if (!isset($fname) || $fname === ""){
+    $errors['fname'] = "First name can't be blank";
+}
+if (!isset($lname) || $lname === ""){
+    $errors['lname'] = "Last name can't be blank";
+}
+if (!isset($email) || $email === ""){
+    $errors['email'] = "Email can't be blank";
+}
+
 ?>
+
+<!---------------- Contact Us ---------------->
 
 <header>
     <h1>Contact Us</h1>
@@ -52,9 +67,11 @@ $contact_form = <<<EOF
             </fieldset>
             <textarea rows="7" cols="63" name="question" placeholder="Your message to us :)">$question</textarea>
             <p>
-                Would you like to sign up for our newsletter?
+                Would you like to sign up for:
             </p>
-            <input type="checkbox" name="newsletter" $c_newsletter>Yes
+            <input type="checkbox" name="newsletter" $c_newsletter>Our newsletter
+            <br>
+            <input type="checkbox" name="notify" $c_notify>Notify me of new boxers to adopt
             <p>
                 How would you prefer to be contacted?
             </p>
@@ -85,6 +102,22 @@ EOF;
     </div>
     <div class="col-6 contactright">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2696.8640314680683!2d-122.21819999999998!3d47.473079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905d4cb95a3c11%3A0xf6acc28a6e5b393a!2sPopeyes!5e0!3m2!1sen!2sus!4v1433891300348" width="400" height="300" style="border:0"></iframe>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <?php
+        if (count($errors) > 0){
+            echo "<h4> Please fix the following errors </h4>";
+            echo "<ul>";
+            foreach ($errors as $key => $error){
+                echo "<li>$error</li>";
+            }
+            echo "</ul>";
+        }
+
+        ?>
     </div>
 </div>
 
