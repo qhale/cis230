9252f61b7af6295a9b07b222c3fd1fe5cae3899e
Queena <Queena@Queenas-MacBook-Air.local>
2016-02-17 12:18:15 -0800

    Added password crud

--- includes/utilities.php
+++ includes/utilities.php
@@ -13,6 +13,23 @@ function createSelect($name, array $a, $selected_inquiry){
     return $select;
 }
 
+$lower = implode(range('a','z'));
+$upper = implode(range('A','Z'));
+$number = implode(range(0, 9));
+$symbols='$***!@!';
+$chars=$lower.$upper.$numbers.$symbols;
 
+function random_char($string){
+    $i = mt_rand(0,strlen($string) -1);
+    return $string[$i];
+}
+
+function temp_password($length, $chars){
+    $temp_pw = '';
+    for($i = 0; $i < $length; $i++){
+        $temp_pw .= random_char($chars);
+    }
+    return $temp_pw;
+}
 
 ?>
---
+++ password_functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Queena
+ * Date: 2/16/16
+ * Time: 3:58 AM
+ */
