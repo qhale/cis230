b24ce11a51d5ef21d8ed9bd3ea097eca9deea4fb
Queena <Queena@Queenas-Air.hsd1.wa.comcast.net>
2016-02-17 22:21:22 -0800

    Checks database for an email match then logs in If email and password does not match, shows login failed

--- login.php
+++ login.php
@@ -1,11 +1,13 @@
 <?php
+ob_start();
+
 $title ='Login';
 require 'includes/head.php';
 require 'includes/nav.php';
 require 'includes/data_connection.php';
 
 $user_email = $_POST['email'];
-$password = $_POST['password'];
+$readable_pw = $_POST['readable_pw'];
 $submit = $_POST['submit'];
 
 if ($submit) {
@@ -13,9 +15,8 @@ if ($submit) {
     $sql = "SELECT * FROM users WHERE email='$user_email'";
     $result = $db->query($sql);
     list($user_id, $f_name, $l_name, $user_email, $password, $role) = $result->fetch_row();
-
-    if ($user_email) {
-        echo "<br> Logged In";
+    
+    if ($readable_pw == $password){
         $_SESSION['f_name']=$f_name;
         ob_clean();
         header("Location: story_admin.php");
@@ -23,9 +24,7 @@ if ($submit) {
     else {
         echo "<br>Login Failed";
     }
-
 }
-
 ?>
 
 
@@ -44,7 +43,7 @@ $login = <<<LOGIN
         <label>Username</label>
         <input type="text" name="email" value="$user_email"><br>
         <label>Password</label>
-        <input type="password" name="password" value="$password"><br>
+        <input type="password" name="readable_pw" value="$readable_pw"><br>
         <input type="submit" name="submit" value="Login">
         <a class="login-forgot-pw link-on-white-back" href="#">Forgot your password?</a>
     </fieldset>
