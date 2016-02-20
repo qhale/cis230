4c22b0c030a73812c3249fdbd82e2d93d4579a3d
Queena <Queena@Queenas-Air.hsd1.wa.comcast.net>
2016-02-19 21:47:15 -0800

    Created user_delete file

---
+++ user_delete.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+$title = 'User Delete';
+require 'includes/data_connection.php';
+
+$user_id=$_GET['user_id'];
+$sql = "DELETE FROM stories WHERE user_id=$user_id";
+$result = $db ->query($sql);
+mysqli_close($db);
+$now = time();
+ob_clean();
+header("Location: user_admin.php?t=$now&confirm=deleted");
+
+?>
